75f23b28c229d897430a2e7e27dc737bf4476aaa
Nico Almena <fnalmenaradumur96@gmail.com>
2018-09-20 19:04:23 -0400

    In my working main.js ive made it so we can search bands and get their info

---
+++ working_main.js
@@ -0,0 +1,28 @@
+// On click event- handles displaying band info from Last.FM (image, summary & link)
+$(document).on("click", "#userInput", function (event) {
+
+    // Prevents page from reloading
+    event.preventDefault();
+    
+    // New last.FM AJAX Call to retrieve band info
+    var bandInfo = $("#bandInput").val();
+    console.log(bandInfo);
+    var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + bandInfo + "&api_key=91228e136aa2fa9726fb4de5c9da5b81&format=json";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    
+    // Response handler from Last.FM
+    }).then(function (response) {
+        console.log(response);
+        //console.log(response.artist.bio.content);
+        var bandBio = response.artist.bio.content;
+        console.log(bandBio);
+        // var bandPic = response.artist.image[3]["#text"];
+        // var bandBio = response.artist.bio.summary;
+        // console.log(bandPic);
+        // console.log(bandBio)
+        
+    });
+});
+
